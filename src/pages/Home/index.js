07263128fd0ff8cf52e39427c15dbb23b4e399d3
Vinicius Rocha <viniciusrocha@Vini-MacBook.local>
2019-08-15 14:58:25 -0400

    minor fix

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,6 @@ import React from "react";
 
 //components
 import Sidebar from "../../components/Sidebar";
-import UserInfoCard from "../../components/UserInfoCard";
 import Dashboard from "../../components/Dashboard";
 
 class Home extends React.Component {
